7ae68ebc24d77df296e5daef26d723c1e0fb7e98
De Filippis Francesco <defilippis@omicronconsulting.it>
2025-08-03 12:45:00 +0200

    add lib-ui package

---
+++ packages/lib-ui/src/lib/lib-ui.tsx
@@ -0,0 +1,11 @@
+import styles from './lib-ui.module.css';
+
+export function GigSportXLibUi() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to GigSportXLibUi!</h1>
+    </div>
+  );
+}
+
+export default GigSportXLibUi;
